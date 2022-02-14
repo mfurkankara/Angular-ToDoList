e82c73d728b4da14ecbfa634fb4066610eb5f1ec
mfurkankara <mfurkankara_3210@outlook.com>
2022-02-14 17:08:56 +0300

    aws #api

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,7 +10,7 @@ import { isNgTemplate } from '@angular/compiler';
 })
 export class ListComponent implements OnInit {
   todolist: Todos[];
-  deneeff
+  deneefffff
   constructor(private todolistservice: TodosService) { }
 
   // public GetAll() {
